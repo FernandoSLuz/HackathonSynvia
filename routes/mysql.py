040c677b321846cef0905a99e7a65f46f217401a
FernandoSLuz <fernandosilvadaluz@gmail.com>
2019-08-18 05:16:46 -0300

    mysql improvements_2

--- routes/mysql.py
+++ routes/mysql.py
@@ -5,6 +5,14 @@ import flask
 from flask import request
 
 blueprint = flask.Blueprint('mysql', __name__)
+
+
 @blueprint.route('/mysql_insert', methods=[ 'GET', 'POST' ])
-def dialog():
+def mysql_insert():
+    form = request.get_json(silent=True, force=True)
     return {'fulfillmentText': "insert"}
+
+@blueprint.route('/mysql_select', methods=[ 'GET', 'POST' ])
+def mysql_select():
+    form = request.get_json(silent=True, force=True)
+    return {'fulfillmentText': "select"}
