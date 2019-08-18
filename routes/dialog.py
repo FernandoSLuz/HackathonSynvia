c901afc7c090d99e7cccd2fd4187a2ee24ef7ede
FernandoSLuz <fernandosilvadaluz@gmail.com>
2019-08-18 04:03:59 -0300

    dialog test_3

--- routes/dialog.py
+++ routes/dialog.py
@@ -11,17 +11,17 @@ blueprint = flask.Blueprint('auth', __name__)
 def dialog():
     
     form = request.get_json(silent=True, force=True)
-    res = (json.dumps(form, indent=4))
-    required_attributes = [ 'displayName', 'queryText']
-
-    for attr in required_attributes:
-        if attr not in form:
-            return flask.jsonify({
-                'fulfillmentText': 'attribute {} required'.format(attr)
-            }), 400
-    displayName, queryText = [ form[key] for key in required_attributes ]
-
-
+    res = (json.dumps(form, indent=3))
     print("Request:" + res)
-    response = displayName + ' ----  queryText: ' + queryText 
-    return {'fulfillmentText': response}
+    if(res is 'null'):
+        return {'fulfillmentText': 'response error'}
+    else:
+        required_attributes = [ 'displayName', 'queryText']
+        for attr in required_attributes:
+            if attr not in form:
+                return flask.jsonify({
+                    'fulfillmentText': 'attribute {} required'.format(attr)
+                }), 400
+        displayName, queryText = [form[key] for key in required_attributes ]        
+        response = displayName + ' ----  queryText: ' + queryText 
+        return {'fulfillmentText': response}
